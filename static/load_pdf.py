1abdb9de1f2dc847faf639395ad37a69b9b3aebd
janabii <ggjanabi@gmail.com>
2025-04-14 19:56:30 +0300

    Delete static directory

--- static/load_pdf.py
+++
@@ -1,8 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader
-
-
-def load_pdf():
-    """Loads a PDF file and extracts text"""
-    loader = PyPDFLoader("data/example.pdf")
-    documents = loader.load()
-    return documents
